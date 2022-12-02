a76f38e68f5b0dbe6bc55324918eac2110825074
Christoffer Bjelke <christoffer.bjelke@outlook.com>
2022-12-02 15:41:32 +0100

    day 2

---
+++ 2022/2/1.py
@@ -0,0 +1,25 @@
+score = 0
+
+my_shape = {"X": 1, "Y": 2, "Z": 3}
+opponent_shape = {"A": 1, "B": 2, "C": 3}
+
+def result(opp, me):
+  if opponent_shape[opp] == my_shape[me]:
+    return 3 + my_shape[me]
+  elif my_shape[me] == 2 and opponent_shape[opp] == 1:
+    return 6 + my_shape[me]
+  elif my_shape[me] == 3 and opponent_shape[opp] == 2:
+    return 6 + my_shape[me]
+  elif my_shape[me] == 1 and opponent_shape[opp] == 3:
+    return 6 + my_shape[me]
+  else:
+    return 0 + my_shape[me]
+
+while True:
+  try:
+    n = input()
+    score += result(n[0], n[2])
+  except EOFError:
+    break
+
+print(score)
---
+++ 2022/2/2.py
@@ -0,0 +1,21 @@
+score = 0
+
+decider = {"X": 0, "Y": 3, "Z": 6}
+opponent_shape = {"A": 1, "B": 2, "C": 3}
+
+def result(opp, me):
+  if decider[me] == 0:
+    return decider[me] + (opponent_shape[opp] - 1 if opponent_shape[opp] - 1 > 0 else 3)
+  elif decider[me] == 3:
+    return decider[me] + opponent_shape[opp]
+  else:
+    return decider[me] + (opponent_shape[opp] + 1 if opponent_shape[opp] + 1 < 4 else 1)
+
+while True:
+  try:
+    n = input()
+    score += result(n[0], n[2])
+  except EOFError:
+    break
+
+print(score)
