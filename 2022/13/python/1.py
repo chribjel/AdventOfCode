f11dbf173adc072fa70f232e0cc4050c35af2ac4
Christoffer Bjelke <christoffer.bjelke@outlook.com>
2022-12-13 07:58:38 +0100

    remove unnecessary code

--- 2022/13/python/1.py
+++ 2022/13/python/1.py
@@ -2,15 +2,6 @@ def is_valid(left, right, i = 0):
 
     if isinstance(left, list):
         if isinstance(right, list):
-            if len(left) == 0 and len(right) > 0:
-                return True
-            elif len(left) > 0 and len(right) == 0:
-                return False
-            elif len(left) == 0 and len(right) == 0:
-                return None
-
-
-            # both are lists
             for i in range(len(left)):
                 try:
                     if is_valid(left[i], right[i], i) == False:
@@ -23,8 +14,6 @@ def is_valid(left, right, i = 0):
 
             if len(left) < len(right):
                 return True
-            elif len(left) == len(right):
-                return None
         else:
             return is_valid(left, [right])
     elif isinstance(right, list):
--- 2022/13/python/2.py
+++ 2022/13/python/2.py
@@ -5,26 +5,12 @@ class Packet:
     def __lt__(self, other):
         return is_valid(self.list, other.list) == True
 
-    def __gt__(self, other):
-        return is_valid(other.list, self.list) == False
-
-    def __eq__(self, other):
-        return is_valid(self.list, other.list) == None
 
 
 def is_valid(left, right, i = 0):
 
     if isinstance(left, list):
         if isinstance(right, list):
-            if len(left) == 0 and len(right) > 0:
-                return True
-            elif len(left) > 0 and len(right) == 0:
-                return False
-            elif len(left) == 0 and len(right) == 0:
-                return None
-
-
-            # both are lists
             for i in range(len(left)):
                 try:
                     if is_valid(left[i], right[i], i) == False:
@@ -37,8 +23,6 @@ def is_valid(left, right, i = 0):
 
             if len(left) < len(right):
                 return True
-            elif len(left) == len(right):
-                return None
         else:
             return is_valid(left, [right])
     elif isinstance(right, list):
