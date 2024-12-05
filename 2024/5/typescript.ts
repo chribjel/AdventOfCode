d108baabbf2c32352ee9e058e6972b6c02a2ffc1
Christoffer Bjelke <christoffer.bjelke@outlook.com>
2024-12-05 14:20:47 +0100

    add timings for fun

--- 2024/5/typescript.ts
+++ 2024/5/typescript.ts
@@ -1,9 +1,63 @@
+const ITERATIONS = 100;
+
+const exampleInput = await getExampleInput();
 const input = await getInput();
 
-console.log("Part 1 (example):", part1(await getExampleInput()));
-console.log("Part 1:", part1(input));
-console.log("Part 2 (example):", part2(await getExampleInput()));
-console.log("Part 2:", part2(input));
+function timeSolution(solution: () => number) {
+  const start = Bun.nanoseconds();
+  const result = solution();
+  const end = Bun.nanoseconds();
+  const ms = (end - start) / 1_000_000;
+  return [result, ms];
+}
+
+function standardDeviation(arr: number[]) {
+  const avg = arr.reduce((a, b) => a + b, 0) / arr.length;
+  const squareDiffs = arr.map((a) => (a - avg) ** 2);
+  const avgSquareDiff = squareDiffs.reduce((a, b) => a + b, 0) / arr.length;
+  return Math.sqrt(avgSquareDiff);
+}
+
+// run solutions 1000 times and average the result and calculate delta
+const times1: number[] = [];
+const times2: number[] = [];
+let solution1 = 0;
+let solution2 = 0;
+for (let i = 0; i < ITERATIONS; i++) {
+  const [result1, ms1] = timeSolution(() => part1(input));
+  times1.push(ms1);
+  const [result2, ms2] = timeSolution(() => part2(input));
+  times2.push(ms2);
+  if (i === 0) {
+    solution1 = result1;
+    solution2 = result2;
+  }
+}
+const ms1 = times1.reduce((a, b) => a + b, 0) / ITERATIONS;
+const ms1TwoDecimals = ms1.toFixed(2);
+const ms2 = times2.reduce((a, b) => a + b, 0) / ITERATIONS;
+const ms2TwoDecimals = ms2.toFixed(2);
+
+const sd1 = standardDeviation(times1);
+const diff1 = ms1 - Math.min(...times1);
+const diff1TwoDecimals = diff1.toFixed(2);
+const sd1TwoDecimals = sd1.toFixed(2);
+const sd2 = standardDeviation(times2);
+const diff2 = ms2 - Math.min(...times2);
+const diff2TwoDecimals = diff2.toFixed(2);
+const sd2TwoDecimals = sd2.toFixed(2);
+
+console.log("-------------------------------------");
+console.log("PART 1:");
+console.log("Solution:", solution1);
+console.log("Time:", `${ms1TwoDecimals}ms +-`, sd1TwoDecimals);
+console.log("Samples:", times1.length);
+console.log("-------------------------------------");
+console.log("PART 2:");
+console.log("Solution:", solution2);
+console.log("Time:", `${ms2TwoDecimals}ms +-`, sd2TwoDecimals);
+console.log("Samples:", times2.length);
+console.log("-------------------------------------");
 
 function getRules(input: string) {
   const [rules] = input.split("\n\n");
@@ -134,7 +188,6 @@ function part2(input: string) {
   let result = 0;
 
   for (let i = 0; i < updates.length; i++) {
-    console.log(`update ${i + 1}/${updates.length}`);
     const update = updates[i].split(",");
 
     if (isUpdateValid(update, ruleMap)) {
